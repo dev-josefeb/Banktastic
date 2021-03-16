5ec299afd82a8e4adcf61b614ac907ccd544ad47
dev-josefeb <febinjose@protonmail.com>
2021-03-16 17:26:18 +0100

    Add basic data for the app

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const account1 = {
+  owner: 'John Smith',
+  transactions: [500, -350, 560, 800, -60, 1130, 2270, -300],
+  interestRate: 2.1,
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Robert Miller',
+  transactions: [1000, 200, -150, 5790, 525, -1000, 10500, -90],
+  interestRate: 1.6,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Sarah Jones',
+  transactions: [500, -100, -40, 600, -50, 95, 455, -65],
+  interestRate: 3.75,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Marta James Peterson',
+  transactions: [1430, 900, -700, 250, -90],
+  interestRate: 0.4,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
